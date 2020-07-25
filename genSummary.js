2a2cae3d2aaad8ef5ef263511de9d8aed995ab1a
John Park <loopuntiltrue@gmail.com>
2020-07-25 16:25:25 +0900

    Finished testing out the model for how the scrapers should behave as well as establishing how elements should be loaded.

--- genSummary.js
+++ genSummary.js
@@ -1,16 +1,58 @@
-// MUST SPLIT INTO TWO SECTIONS:
-// One that does that scraping and parsing and
-// One that does the delivery to the page.
+// After some thought, I think that the best approach is to make everything "onclick", because otherwise we run into issues with rate limiting.
+// Since we're segmenting the sections out, this section should only contain scripts to build out the page based on JSON returned from scraping.js.
 
-// Injecting HTML into the page.
-let page = chrome.runtime.getURL("summary.html");
-fetch(page)
-.then(response => response.text())
-.then(data=>{
-    var doc = new DOMParser().parseFromString(data, "text/html");
-    var reportContainer = doc.getElementById("reportContainer");
-    document.body.appendChild(reportContainer);
-})
+// Injecting HTML using JS
+// Building the background
+let background = document.createElement("div");
+background.id = "mainContainer";
+// Smaller report container
+let reportContainer = document.createElement("div");
+reportContainer.id = "reportContainer";
+background.appendChild(reportContainer);
+//Header
+let h = document.createElement("h1");
+var t = document.createTextNode("Summary");
+h.appendChild(t);
+reportContainer.appendChild(h);
+//Contents
+let contents = document.createElement("ul");
+contents.id = "courseList";
+reportContainer.appendChild(contents);
+//Adding generated HTML to document.body
+document.body.appendChild(background);
+
+// These collection of functions is designed to build out the info bit by bit, dynamically.
+// I should possibly look to the old callback use of "next" as a model as to how to asynchronously build out the interface.
+// Or a recursive function for stacking promises.
+// For now, I'm just going to stick with designing onclick stuff.
+function showGrades(){
+    
+}
+
+function showAvailable(e){
+    let courseID = e.target.id;
+    whatisAvailable(courseID)
+    .then((d)=>{
+        console.log(d);
+    })
+}
+
+// Building out the course presentation
+// Adding a button to each 
+courseAndGrades()
+.then(pCourseList=>{
+    //Adding items to the courselist
+    for (let pCourse of pCourseList){
+        let item = document.createElement("li");
+        item.id = pCourse.id;
+        item.appendChild(document.createTextNode(pCourse.title));
+        // Nesting another <ul> so that attributes can be added in onclick.
+        item.onclick = () => alert("testing");
+        document.querySelector("#courseList").appendChild(item)
+    }
+});
+
+
 
 //Creating a listener to make itself visible.
 chrome.runtime.onMessage.addListener(
@@ -21,60 +63,21 @@ chrome.runtime.onMessage.addListener(
         }
 });
 
-// Lets start adding elements onto the Summary page
-const observer = new MutationObserver(() => {
-    var courseList = document.querySelector(".courseListing").querySelectorAll("li");
-    var reportContainer = document.getElementById("reportContainer");
-    var newList = document.createElement("ul");
-    for (var course of courseList){
-        var newItem = document.createElement("li");
-        newItem.innerText=course.textContent;
-        newList.appendChild(newItem);
-    }
-    reportContainer.appendChild(newList);
-    console.log(courseList);
-    console.log(courseList[0].querySelector("a").href);
-    var embeddedOpen = document.createElement("iframe");
-    embeddedOpen.src = courseList[0].querySelector("a").href;
-    document.body.appendChild(embeddedOpen);
-});
-observer.observe(document.getElementById("div_4_1"), {"childList": true});
-
-var grades = document.createElement("iframe");
-grades.src = "https://blackboard.stonybrook.edu/webapps/streamViewer/streamViewer?cmd=view&streamName=mygrades&globalNavigation=false"
-grades.id = "grades_iframe";
-document.body.appendChild(grades)
-// Making promises to make sure that we're looking at the right spot.
-new Promise((resolve, reject) => {
-    var checkGrades = setInterval(()=>{
-        console.log("first");
-        console.log(grades);
-        if (grades.contentWindow.document.body.querySelector(".stream_item")){
-            clearInterval(checkGrades);
-            console.log(grades.contentWindow.document.body.querySelectorAll(".stream_item"))
-            var grades_iframe = grades.contentWindow.document.body.querySelector("#mybbCanvas");
-            console.log(grades_iframe);
-            resolve(grades_iframe);
-        }
-    }, 1000)
-})
-// .then((firstFrame) => {
-//     console.log("second one");
-//     console.log(firstFrame);
-//     var grades_iframe = grades.contentWindow.document.body.querySelector("#mybbCanvas");
-//     console.log(grades_iframe)
-//     var checkGradeList = setInterval(()=>{
-//         console.log("second");
-//         console.log(firstFrame);
-//         var courseList = firstFrame.contentWindow.document.body.querySelectorAll(".stream_item")
-//         if (firstFrame.contentWindow.document.body.querySelectorAll(".stream_item").length > 0) {
-//             console.log(grades.querySelectorAll("stream_item"));
-//         }
-//         clearInterval(checkGradeList);
-//     }, 1000);
-// })
-
-// Seperate functions for parsing each page. This is going to be a bitch.
-
-// Functions with callbacks so that elements are added to the main page as they are loaded.
-
+// // Lets start adding elements onto the Summary page
+// const observer = new MutationObserver(() => {
+//     var courseList = document.querySelector(".courseListing").querySelectorAll("li");
+//     var reportContainer = document.getElementById("reportContainer");
+//     var newList = document.createElement("ul");
+//     for (var course of courseList){
+//         var newItem = document.createElement("li");
+//         newItem.innerText=course.textContent;
+//         newList.appendChild(newItem);
+//     }
+//     reportContainer.appendChild(newList);
+//     // console.log(courseList);
+//     // console.log(courseList[0].querySelector("a").href);
+//     var embeddedOpen = document.createElement("iframe");
+//     embeddedOpen.src = courseList[0].querySelector("a").href;
+//     document.body.appendChild(embeddedOpen);
+// });
+// observer.observe(document.getElementById("div_4_1"), {"childList": true});
